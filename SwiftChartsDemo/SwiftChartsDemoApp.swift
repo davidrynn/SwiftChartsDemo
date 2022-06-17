9e70fd1bb0d3211b767ed143b0d59805bd04dba3
David Rynn <davidrynn@gmail.com>
2022-06-17 16:48:41 -0400

    Initial Commit

---
+++ SwiftChartsDemo/SwiftChartsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChartsDemoApp.swift
+//  SwiftChartsDemo
+//
+//  Created by David Rynn on 6/17/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChartsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
